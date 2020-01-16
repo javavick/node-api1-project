5f37c827ff4bfc0b4eb86ef58e203ba2311874f3
Vick Javariz <51038821+javavick@users.noreply.github.com>
2020-01-16 05:00:52 -0400

    Add GET Request

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// implement your API here
+const express = require("express");
+const Data = require("./data/db.js");
+
+const server = express();
+const port = 8000;
+
+server.use(express.json());
+
+// GET REQUEST ("/")
+server.get("/api/users", (req, res) => {
+  Data.find()
+    .then((data) => res.status(200).json(data))
+    .catch(() =>
+      res.status(500).json({
+        errorMessage: "Sorry, we ran into an error when getting the data."
+      })
+    );
+});
+
+server.listen(port, () => console.log(`\n *** API ON PORT: ${port} *** \n`));
