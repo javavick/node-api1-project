325833f0da137cb4e31da90a026a559a571a7a80
Vick Javariz <51038821+javavick@users.noreply.github.com>
2020-01-17 01:54:51 -0400

    Add GET Request for '/api/users/:id' and POST Request for '/api/users/'

--- index.js
+++ index.js
@@ -6,15 +6,59 @@ const port = 8000;
 
 server.use(express.json());
 
-// GET REQUEST ("/")
+// GET REQUEST ("/api/users")
 server.get("/api/users", (req, res) => {
   Data.find()
     .then((data) => res.status(200).json(data))
     .catch(() =>
       res.status(500).json({
-        errorMessage: "Sorry, we ran into an error when getting the data."
+        errorMessage: "The users information could not be retrieved."
       })
     );
+});
+
+// GET REQUEST ("/api/users/:id")
+server.get(`/api/users/:id`, (req, res) => {
+  Data.findById(req.params.id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).json({
+          errorMessage: "The user with the specified ID does not exist."
+        });
+      } else {
+        res.status(200).json(data);
+      }
+    })
+    .catch(() =>
+      res.status(500).json({
+        errorMessage: "The user information could not be retrieved."
+      })
+    );
+});
+
+// POST REQUEST ("/api/users")
+server.post("/api/users", (req, res) => {
+  Data.insert(req.body)
+    .then((data) => {
+      if (!req.body.name || !req.body.bio) {
+        res
+          .status(400)
+          .json({ errorMessage: "Please provide name and bio for the user" });
+      } else {
+        Data.findById(data.id)
+          .then((data) => res.status(201).json(data))
+          .catch(() =>
+            res.status(500).json({
+              errorMessage: "The users information could not be retrieved."
+            })
+          );
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        errorMessage: "There was an error while saving the user to the database"
+      });
+    });
 });
 
 server.listen(port, () => console.log(`\n *** API ON PORT: ${port} *** \n`));
